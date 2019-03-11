a6410f39f02465fd80dd9a3fc340205d01793145
Diogo Farinella <vitorfransine@gmail.com>
2019-03-11 14:56:13 -0300

    Adiciona lib mat

--- main.c
+++ main.c
@@ -1,8 +1,10 @@
 #include<stdio.h>
+#include "matematica.h"
 
 int main()
 {
-    printf("Hello World!\n");
+    int r=soma(1,3);
+    printf("r=%d\n",r);
     printf("Natan não sai do prata esse ano!\n");
     return 0;
 } 
---
+++ matematica.h
@@ -0,0 +1,3 @@
+//biblioteca matematica mito
+
+int soma(int a,int b);
